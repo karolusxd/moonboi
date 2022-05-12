e48a1b1862fb88ec2feb714e31095d624db85f58
とも <tomoyoshi.usa@gmail.com>
2022-05-12 19:13:56 +0900

    1

---
+++ AOJ0521.py
@@ -0,0 +1,12 @@
+moonboy = True
+while (moonboy):
+    lol = 1000-int(input())
+    if lol == 1000: break
+    thecoinzzz = [500,100,50,10,1]
+    howmanycoinzzzmyboiiiii = 0
+    for a in thecoinzzz:
+        while lol >= a:
+            lol -= a
+            howmanycoinzzzmyboiiiii += 1
+            
+    print(howmanycoinzzzmyboiiiii)
